0d73f116155f13b94ed0b461112356910eb061ae
dariomtz <gusdamtzr@gmail.com>
2022-04-27 00:26:20 -0500

    Encripted file sending

--- tcp_client.py
+++ tcp_client.py
@@ -1,16 +1,43 @@
-import base64
 import socket
+import nacl.secret
+import nacl.utils
+from nacl.bindings import sodium_increment
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
+CHUNK_SIZE = 1024
 
 
 def main() -> None:
+    key = nacl.utils.randombytes_deterministic(
+        nacl.secret.SecretBox.KEY_SIZE,
+        b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"
+        b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF",
+    )
+
+    box = nacl.secret.SecretBox(key)
+    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
+        # Send nonce once at the beginning
+        s.sendall(nonce)
         with open("./data/client/file.txt", "rb") as file:
-            for line in file.readlines():
-                s.sendall(line)
+            while True:
+                # read file in chunks instead of lines to be consistent with size of
+                # encryption and decription
+                chunk = file.read(CHUNK_SIZE)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += bytes(" " * (16 - (len(chunk) % 16)), "utf-8")
+
+                # encrypt nonce
+                data = box.encrypt(chunk, nonce).ciphertext
+                # send encrypted data
+                s.sendall(data)
+                # increment nonce to get a new one
+                nonce = sodium_increment(nonce)
 
 
 if __name__ == "__main__":
--- tcp_server.py
+++ tcp_server.py
@@ -1,24 +1,46 @@
-import base64
 import socket
-import custom_random
+import nacl.utils
+import nacl.secret
+from nacl.bindings import sodium_increment
+
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+CHUNK_SIZE = 1024
 
 
 def main() -> None:
+    key = nacl.utils.randombytes_deterministic(
+        nacl.secret.SecretBox.KEY_SIZE,
+        b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"
+        b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF",
+    )
+
+    box = nacl.secret.SecretBox(key)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
+            # receive nonce to decrypt
+            nonce = conn.recv(nacl.secret.SecretBox.NONCE_SIZE)
+
             file_data = b""
             while True:
-                data = conn.recv(1024)
-                if not data:
+                # receive encrypted data
+                # size of this is chunk + mac size
+                data = conn.recv(CHUNK_SIZE + box.MACBYTES)
+                if len(data) == 0:
                     break
-                file_data += data
+                elif len(data) % 16 != 0:
+                    data += bytes(" " * (16 - (len(data) % 16)), "utf-8")
+
+                # decrypt using nonce
+                file_data += box.decrypt(data, nonce)
+                # update nonce so that is same as in client
+                nonce = sodium_increment(nonce)
 
     with open("./data/server/received_file.txt", "wb") as file:
         file.write(file_data)
